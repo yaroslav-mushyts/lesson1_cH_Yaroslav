11ef5e53d921c789ac97fce3b9ad5133736743c5
Yarik Mushyts <swat.www.ru.56@mail.ru>
2024-02-15 15:13:14 +0300

    add lesson1.Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 4;
+int b = 6;
+int c = 3;
+int d = 9;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
