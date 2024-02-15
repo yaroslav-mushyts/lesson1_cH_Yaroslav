e67135dadffbd2090b2f8b6b68120efb6ff5636e
Yarik Mushyts <swat.www.ru.56@mail.ru>
2024-02-15 17:51:57 +0300

    add lesson1.DZ

---
+++ DZ/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 4;
+int b = 6;
+int sum = a * b;
+Console.WriteLine(sum);
+Console.WriteLine(' ');
+
+int c = 7;
+int d = 10;
+int sum1 = c + d;
+Console.WriteLine(sum1);
+Console.WriteLine(' ');
+
+
+int qoutient = sum / sum1;
+Console.WriteLine(qoutient);
